4ff34113ebcb53fdc1c4956c4a61412b67671950
Nikita Sharma <36643628+nikitaa30@users.noreply.github.com>
2019-07-05 00:24:51 +0530

    Update recommender_system.py

--- recommender_system.py
+++ recommender_system.py
@@ -15,25 +15,19 @@ for idx, row in ds.iterrows():
     similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
     similar_items = [(cosine_similarities[idx][i], ds['id'][i]) for i in similar_indices]
 
-    # First item is the item itself, so remove it.
-    # Each dictionary entry is like: [(1,2), (3,4)], with each tuple being (score, item_id)
     results[row['id']] = similar_items[1:]
     
 print('done!')
 
-# hacky little function to get a friendly item name from the description field, given an item ID
 def item(id):
     return ds.loc[ds['id'] == id]['description'].tolist()[0].split(' - ')[0]
 
-# Just reads the results out of the dictionary. No real logic here.
+# Just reads the results out of the dictionary.
 def recommend(item_id, num):
     print("Recommending " + str(num) + " products similar to " + item(item_id) + "...")
     print("-------")
     recs = results[item_id][:num]
     for rec in recs:
         print("Recommended: " + item(rec[1]) + " (score:" + str(rec[0]) + ")")
-
-# Just plug in any item id here (1-500), and the number of recommendations you want (1-99)
-# You can get a list of valid item IDs by evaluating the variable 'ds', or a few are listed below
 
 recommend(item_id=11, num=5)
